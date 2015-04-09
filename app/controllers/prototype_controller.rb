c0362ef9f255325fd1f024fc9607bdf61e88ba31
FunnyQ <funnyq@funnyq.com>
2015-04-10 00:22:23 +0800

    [add] setting up basic structure

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,9 +1,9 @@
-  class PrototypeController < ApplicationController
+class PrototypeController < ApplicationController
 
-    def index
-      @username = '郭柏廷'
-      @level = rand 99
-      @money = rand 9999
-    end
-
+  def index
+    @username = '郭柏廷'
+    @level = rand 99
+    @money = rand 9999
   end
+
+end
