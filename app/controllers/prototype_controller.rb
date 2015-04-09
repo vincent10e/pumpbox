3445ee24a87b6e3f4fcf6a1f86d18a6de74898e0
FunnyQ <funnyq@funnyq.com>
2015-04-10 01:58:40 +0800

    [add] adding skill list

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -6,6 +6,21 @@ class PrototypeController < ApplicationController
     @money = rand 999
     @school = '中山國中'
     @grade = '八年級'
+    @skills = {
+        title: '初階測距術',
+        learned: [
+          '角度測量',
+          '內角與外角',
+          '多邊形內角'
+        ]
+      },{
+        title: '圓與圓的關係',
+        learned: [
+          '相切',
+          '連心線',
+          '公切線'
+        ]
+      }
   end
 
 end
