f6b581f57c4b554b97e812b3d64d63b1d4a73c5b
FunnyQ <funnyq@funnyq.com>
2015-04-10 03:52:59 +0800

    [add] adding course path section

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -1,6 +1,12 @@
 class PrototypeController < ApplicationController
 
   def index
+    fake_data
+  end
+
+  private
+
+  def fake_data
     @username = '郭柏廷'
     @level = rand 9
     @money = rand 999
@@ -17,9 +23,53 @@ class PrototypeController < ApplicationController
         title: '圓與圓的關係',
         learned: [
           '相切',
+          '連心線'
+        ]
+      }
+    @courses = {
+        title: '初階測距術',
+        status: 2, # 0: locked, 1: available, 2: passed
+        description: '敘述',
+        sub_titles: [
+          '子關卡1',
+          '子關卡2',
+          '子關卡3'
+        ],
+        exp: 3000,
+        money: 1
+      },{
+        title: '圓與圓的關係',
+        status: 1, # 0: locked, 1: available, 2: passed
+        description: '了解圓與圓之間的基本關係，未來可以應用到齒輪設計等機械相關技能，可以設計出更省時省力的工具！',
+        sub_titles: [
+          '相切',
           '連心線',
           '公切線'
-        ]
+        ],
+        exp: 3000,
+        money: 1
+      },{
+        title: '切線應用',
+        status: 0, # 0: locked, 1: available, 2: passed
+        description: '敘述',
+        sub_titles: [
+          '子關卡1',
+          '子關卡2',
+          '子關卡3'
+        ],
+        exp: 3000,
+        money: 1
+      },{
+        title: '切線應用',
+        status: 0, # 0: locked, 1: available, 2: passed
+        description: '敘述',
+        sub_titles: [
+          '子關卡1',
+          '子關卡2',
+          '子關卡3'
+        ],
+        exp: 3000,
+        money: 1
       }
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,15 @@ module ApplicationHelper
     image_tag "http://placehold.it/#{width}x#{height}&text=#{text}", :class => "#{classes}"
   end
 
+  def render_course_status(course_status_code)
+    case course_status_code
+    when 0
+      raw '<i class="fa fa-lock"></i>'
+    when 1
+      raw '<i class="fa fa-circle available"></i>'
+    when 2
+      raw '✔'
+    end
+  end
+
 end
