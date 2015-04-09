7c66d2770a5315c19471879368b4f7d4096bd56d
FunnyQ <funnyq@funnyq.com>
2015-04-10 04:26:18 +0800

    [add] adding course title and video block

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,16 @@ module ApplicationHelper
     end
   end
 
+  def youtube_embed(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      # Regex from # http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+
+    raw %Q{<iframe class="video-wrapper" title="YouTube video player" src="http://www.youtube.com/embed/#{ youtube_id }?rel=0&amp;frameborder="0" allowfullscreen></iframe>}
+  end
+
 end
