6e8888d713f000cfa05df8ef550513c1a56890e2
FunnyQ <funnyq@funnyq.com>
2015-04-10 01:06:08 +0800

    [add] adding personal info block partial

--- app/controllers/prototype_controller.rb
+++ app/controllers/prototype_controller.rb
@@ -2,8 +2,10 @@ class PrototypeController < ApplicationController
 
   def index
     @username = '郭柏廷'
-    @level = rand 99
-    @money = rand 9999
+    @level = rand 9
+    @money = rand 999
+    @school = '中山國中'
+    @grade = '八年級'
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def image_placeholder(args)
+    width = args[:width] || '300'
+    height = args[:height] || '300'
+    text = args[:text] || "#{width} x #{height}"
+    classes = args[:class] || ""
+
+    image_tag "http://placehold.it/#{width}x#{height}&text=#{text}", :class => "#{classes}"
+  end
+
 end
