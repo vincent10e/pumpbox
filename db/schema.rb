191e70bd0989d92dd9f0962939d3dee9e68f1018
FunnyQ <funnyq@funnyq.com>
2015-05-16 10:57:45 +0800

    [update] update database.yml
     - 更新 shared/application.yml
     - 更新 shared/database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -14,76 +14,76 @@
 ActiveRecord::Schema.define(version: 20150509074221) do
 
   create_table "chapters", force: :cascade do |t|
-    t.integer  "volume_number"
-    t.integer  "subject_id"
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "volume_number", limit: 4
+    t.integer  "subject_id",    limit: 4
+    t.string   "title",         limit: 255
+    t.text     "description",   limit: 65535
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "concepts", force: :cascade do |t|
-    t.integer  "subject_id"
-    t.integer  "volume_number"
-    t.integer  "chapter_id"
-    t.string   "title"
-    t.text     "description"
-    t.string   "video"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer  "subject_id",    limit: 4
+    t.integer  "volume_number", limit: 4
+    t.integer  "chapter_id",    limit: 4
+    t.string   "title",         limit: 255
+    t.text     "description",   limit: 65535
+    t.string   "video",         limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "students", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "username"
-    t.string   "school"
-    t.integer  "grade"
-    t.string   "phone"
-    t.string   "parent_name"
-    t.string   "parent_phone"
-    t.string   "parent_occupation"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "username",               limit: 255
+    t.string   "school",                 limit: 255
+    t.integer  "grade",                  limit: 4
+    t.string   "phone",                  limit: 255
+    t.string   "parent_name",            limit: 255
+    t.string   "parent_phone",           limit: 255
+    t.string   "parent_occupation",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "grade_cd"
+    t.integer  "grade_cd",               limit: 4
   end
 
-  add_index "students", ["email"], name: "index_students_on_email", unique: true
-  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+  add_index "students", ["email"], name: "index_students_on_email", unique: true, using: :btree
+  add_index "students", ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true, using: :btree
 
   create_table "subjects", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "username"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "username",               limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "school"
-    t.integer  "grade"
+    t.string   "school",                 limit: 255
+    t.integer  "grade",                  limit: 4
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
