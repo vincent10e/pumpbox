2e19da3decfe93c8010f02a8484310a055ca8c24
Vincent <vincent10e@gmail.com>
2015-06-01 16:56:00 +0800

    remove unrequired migration file

--- db/migrate/20150501061253_add_school_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddSchoolToUser < ActiveRecord::Migration
-  def change
-  	add_column :users, :school, :string
-  	add_column :users, :grade, :integer
-  end
-end
--- db/migrate/20150508154500_add_grade_column_to_student.rb
+++
@@ -1,5 +0,0 @@
-class AddGradeColumnToStudent < ActiveRecord::Migration
-  def change
-  	add_column :students, :grade_cd, :integer
-  end
-end
