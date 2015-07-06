c908c14cc893d73bf5674c1641040b7a6826f7f0
Vincent <vincent10e@gmail.com>
2015-07-06 14:17:58 +0800

    motify the table display of the report page

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -3,9 +3,13 @@ module CoursesHelper
 	# show test_attemp or test_paper last retry_time
 	def show_retry_time(concept, user_id) 
     if !concept.test_attempts.where(user: user_id).last.nil? 
-    	link_to(concept.test_attempts.where(user: user_id).last.retry_time , customized_concept_student_detail_path(concept, user_id))
+    	link_to customized_concept_student_detail_path(concept, user_id) do
+    		content_tag(:div, concept.test_attempts.where(user: user_id).last.retry_time)
+    	end
 		elsif !concept.test_papers.where(user: user_id).last.nil?
-			link_to(concept.test_papers.where(user: user_id).last.retry_time , customized_concept_student_detail_path(concept, user_id)) 
+			link_to customized_concept_student_detail_path(concept, user_id) do
+				content_tag(:div, concept.test_papers.where(user: user_id).last.retry_time)
+			end
 		else
 			0
 		end
