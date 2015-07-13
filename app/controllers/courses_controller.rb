ce23511c4e4357f7fd8429eb913c1878a8a09e91
Vincent <vincent10e@gmail.com>
2015-07-13 11:59:58 +0800

    add city select in student signup page, and modify landing page course filter result

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,7 +14,7 @@ class CoursesController < ApplicationController
 
   # GET /courses/1
   # GET /courses/1.json
-  def showx
+  def show
     @course = Course.find(params[:id])
     @customized_concepts = @course.customized_concepts
 
