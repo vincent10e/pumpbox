4773913d83ab71318b3537284a60bad6fc15c1db
Vincent <vincent10e@gmail.com>
2015-07-28 13:52:38 +0800

    modify customized_concept show page

--- app/helpers/customized_concepts_helper.rb
+++ app/helpers/customized_concepts_helper.rb
@@ -1,2 +1,14 @@
 module CustomizedConceptsHelper
+
+  def show_video_or_not(concept)
+    if !concept.video.blank?
+      content_tag(:div, class: "course-video-wrapper") do
+        youtube_embed(concept.video)
+      end
+    end
+  end
+
 end
+
+
+
