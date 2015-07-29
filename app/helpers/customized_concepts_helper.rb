099e21c4ec07e6cc3596eb592810d4be67cbd6d7
Vincent <vincent10e@gmail.com>
2015-07-29 22:20:29 +0800

    modify collect video button

--- app/helpers/customized_concepts_helper.rb
+++ app/helpers/customized_concepts_helper.rb
@@ -8,5 +8,13 @@ module CustomizedConceptsHelper
     end
   end
 
+  def show_collect_video_button(concept)
+    if current_user.collect_videos.find_by_url(concept.video).blank?
+      content_tag(:button, "收藏影片", class: "btn btn-primary", "data-toggle" => "modal", "data-target" => ".collect_video_modal")
+    else
+      content_tag(:button, "已收藏", class: "btn btn-default", disabled: "disabled")    
+    end
+  end
+
 end
 
